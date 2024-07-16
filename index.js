12375ae10333d1b14084d74a10766f844c59e4bf
Luke Swithenbank <luke@dovetail.com>
2024-07-16 11:21:09 +1000

    add a simple hello-world server

---
+++ index.js
@@ -0,0 +1,15 @@
+const http = require("http"); // Loads the http module
+
+const server = http.createServer((request, response) => {
+  response.writeHead(200, {
+    "Content-Type": "text/plain",
+  });
+
+  console.log("Hello, World!");
+  response.write("Hello, World!\n");
+
+  response.end();
+});
+
+console.log("starting server");
+server.listen(1337);

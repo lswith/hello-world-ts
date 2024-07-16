ae1b1942b187f82301bffd561602e72c5b0e1c2e
Luke Swithenbank <luke@dovetail.com>
2024-07-16 11:37:22 +1000

    add pino logger

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-const tracer = require("dd-trace").init();
+const tracer = require("dd-trace").init({
+  logInjection: true,
+});
 const http = require("http"); // Loads the http module
+const pino = require("pino");
+const logger = require("pino-http")({
+  logger: pino(),
+});
 
 const server = http.createServer((request, response) => {
   response.writeHead(200, {
     "Content-Type": "text/plain",
   });
 
-  console.log("Hello, World!");
+  logger(request, response);
   response.write("Hello, World!\n");
-
   response.end();
 });
 
-console.log("starting server");
+pino().info("starting server");
 server.listen(1337);
